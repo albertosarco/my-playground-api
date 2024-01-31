01b34e164060a6d6ca07c24da9d69fdb67e441f0
Jesus Alberto <sarco.jesus@gmail.com>
2024-01-31 20:45:36 +0100

    initial commit

---
+++ src/main/java/org/example/myplaygroundapi/MyPlaygroundApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.myplaygroundapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPlaygroundApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPlaygroundApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/myplaygroundapi/MyPlaygroundApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.myplaygroundapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPlaygroundApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
